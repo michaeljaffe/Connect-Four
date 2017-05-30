ec016b44cb6857f071139911d27b93c113ca202f
18jeffries <18jeffries@da.org>
2017-05-30 14:09:01 -0400

    New Commit
    Added in Posistion class

--- src/Position.java
+++ src/Position.java
@@ -1,17 +1,73 @@
-
+/**
+ * The position class that sets up row and col
+ * @author Jake Jeffries
+ * @version 5/30/17
+ */
 public class Position {
-	private int row;
-	private int column;
+	private	int row;
+	private int col;
 	
-	public Position(){
-		row = -2;
-		column = -2;
+	/**
+	 * constructor that sets the row and column as -1
+	 */
+	public Position() {
+		row = -1;
+		col = -1;
 	}
 	
-	public Position(int r, int c){
+	/**
+	 * constructor that sets the row and col to the correct values
+	 * @param r
+	 * @param c
+	 */
+	public Position(int r, int c) {
 		row = r;
-		column = c;
+		col = c;
 	}
-
 	
+	/**
+	 * returns the row
+	 * @return row
+	 */
+	public int getRow(){
+		return row;
+	}
+	
+	/**
+	 * returns the column
+	 * @return col
+	 */
+	public int getColumn(){
+		return col;
+	}
+	
+	/**
+	 * sets the row to a new value
+	 * @param r
+	 */
+	public void setRow(int r){
+		row = r;
+	}
+	
+	/**
+	 * sets the col to a new  value
+	 * @param c
+	 */
+	public void setCol(int c){
+		col = c;
+	}
+	
+	/**
+	 * Finds if two Positions are equal
+	 * @return true or false depending on if they're equal
+	 */
+	public boolean equals(Object other){
+		if (other instanceof Position) {
+			Position otherPos = (Position)other;
+			if (otherPos.getRow() == this.getRow() && otherPos.getColumn() == this.getColumn()){
+				return true;
+			}
+		}
+		return false;
+	}
 }
